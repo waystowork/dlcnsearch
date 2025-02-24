a79ffcc5a7ed21c453d9fc89ab8b3520dc8be084
数智未来DLCN <zp0539@yeah.net>
2025-02-24 20:11:59 +0800

    Update constants.js
    替换图标获取工具

--- src/core/constants.js
+++ src/core/constants.js
@@ -1,5 +1,5 @@
 // export const FAVICON_API_URL = 'https://api2.misiai.com/favicon/v1?size=260&url=';
 //https://favicon.cccyun.cc
-export const FAVICON_API_URL = "https://favicon.cccyun.cc/";
+export const FAVICON_API_URL = "https://t2.gstatic.cn/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&size=128&url=";
 // export const FAVICON_API_URL =
 // "https://s2.googleusercontent.com/s2/favicons?sz=32&domain=";
